9b770a0c87e1dff8b3514f639e8b48a4d3518818
MD MONGERUL HAQUE <andrewlaraujo70@gmail.com>
2025-12-28 21:09:20 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,14 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-  ],  
-theme: {
-  extend: {
-    fontFamily: {
-      sans: ['"Baloo Da 2"', 'cursive', 'sans-serif'],
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        // এখানে 'Baloo Da 2' ফন্টটি সেট করা হয়েছে
+        sans: ['"Baloo Da 2"', 'cursive', 'sans-serif'],
+      },
     },
   },
-},
+  plugins: [],
+}
