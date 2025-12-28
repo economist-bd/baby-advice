5ab8891b87954da5941fc5c40b3485460facde6e
MD MONGERUL HAQUE <andrewlaraujo70@gmail.com>
2025-12-28 21:07:44 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  ],  
 theme: {
   extend: {
     fontFamily: {
